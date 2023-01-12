a51da253bf977884b095292f495b9e42d24ebcbf
Coco Matthey <coco.matthey@sap.com>
2023-01-12 14:39:29 -0800

    Module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,24 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    private PostRepository postRepository;
     @RequestMapping("/")
     public String listPosts(ModelMap map) {
+        List<Post> postList = postRepository.getAllPosts();
         map.put("title", "Blog Post 1");
+        map.put("posts", postList);
         return "home";
     }
 }
